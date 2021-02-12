6b4d7470303820d71dfb5a6ebf1e229de922428e
daysSinceSkin <65681191+daysSinceSkin@users.noreply.github.com>
2021-02-12 20:01:30 +0100

    Design + bug fixes
    - removed relative time and instead display difference in days,
    - change select labels
    - fixed sorting bug in chrome+safari

--- app.js
+++ app.js
@@ -11,22 +11,55 @@ const champTemplate = a => `<img class='champIcon lazy' src="img/lazy.jpg" data-
 
 for (let c in champion) {
   champion[c].amountSkins = champion[c].length-1
-  // sort skins by release date
-  champion[c] = champion[c].sort((b,a) => new moment(a.date, "DD-MMM-YYYY") - new moment(b.date, "DD-MMM-YYYY"))
 
-  // calculate time since last skins
-  if (champion[c][0].date == "N/A") {
+  // Sort is not working when a champion has an "N/A" release date, so we need to take care of these edges cases first!
+  if (champion[c].some(skin => skin.date == "N/A")) {
+    // find N/A skin and move it to be the first element
+    let idx = champion[c].findIndex(skin => skin.date == "N/A")
+    let NASkin = champion[c][idx]
+    champion[c].splice(idx, 1)
+    champion[c].unshift(NASkin)
+
     champion[c].lastDate = moment().add(1, 'months')
     champion[c].daysSinceLast = "Coming soon"
   } else {
-    champion[c].lastDate = champion[c][0].date
-    champion[c].daysSinceLast = moment(champion[c][0].date, "DD-MMM-YYYY").fromNow()
-  }
+    // sort skins by release date
+    champion[c] = champion[c].sort((b,a) => new moment(a.date, "DD-MMM-YYYY") - new moment(b.date, "DD-MMM-YYYY"))
 
+    // calculate time since last skins
+    champion[c].lastDate = champion[c][0].date
+
+    // really dirty hack to set the "now" date to always be midnight. otherwise the difference behaves weird.
+    let daysDiff = moment(moment().format("DD-MMM-YYYY"), "DD-MMM-YYYY").diff(moment(champion[c][0].date, "DD-MMM-YYYY"), 'days')
+    champion[c].daysSinceLast = formatDaysText(daysDiff)
+    champion[c].relativeTime = moment(champion[c][0].date, "DD-MMM-YYYY").fromNow()
+  }
+  
   // append inital to DOM
   createNodes(c)
 }
 lazyWrapper()
+
+
+
+// ---- functions ----  \\
+
+function formatDaysText (days) {
+  if (days == -1 ) {
+    return "Tomorrow"
+  } else if  ( days == 0 ) {
+    return "Today"
+  } else if ( days == 1 ) {
+    return "Yesterday"
+  } else if ( days <= -2 ) {
+    return "In " + days*-1  + " days"
+  } else {
+    return days + " days ago"
+  }
+  return "Error"
+}
+
+
 
 function createNodes(c) {
     let content = document.createElement("div")
@@ -40,11 +73,19 @@ function select(selectedValue) {
   if (selectedValue == "least") sortFunc = (a,b) => champion[a].amountSkins - champion[b].amountSkins
   if (selectedValue == "newest") sortFunc = (b,a) => new moment(champion[a].lastDate, "DD-MMM-YYYY") - new moment(champion[b].lastDate, "DD-MMM-YYYY")
   if (selectedValue == "oldest") sortFunc = (a,b) => new moment(champion[a].lastDate, "DD-MMM-YYYY") - new moment(champion[b].lastDate, "DD-MMM-YYYY")
-  if (selectedValue == "za") sortFunc = (a,b) => a - b
+  // if (selectedValue == "za") sortFunc = (a,b) => a - b
+  // sort function for za is broken in chrome / edge, but works in firefox...
+  // that's why i use the same sort func for both az and za, but reverse the array if za is selected
+  if (selectedValue == "za") sortFunc = (a,b) => a + b
   if (selectedValue == "az") sortFunc = (a,b) => a + b
 
   keySorted = Object.keys(champion).sort(sortFunc)
   ul.innerHTML = '';
+
+  if (selectedValue == "za") {
+    keySorted = keySorted.reverse()
+  }
+
   for (let key of keySorted) {
     createNodes(key)
   }
